7914e7457eefe67acd55c18f025aacd4129c6936
Miquel Caamaño Salom <kamanyo@gmailcom>
2023-06-16 16:37:57 +0200

    Add comments to code.

--- main.py
+++ main.py
@@ -5,19 +5,40 @@ from credentials import set_creds
 
 
 def run():
-    """Start script."""
+    """Fetch a number of participants from a survey and sends them the invite."""
+
+    # Configuration:
+    # Survey to fetch from.
+    survey_id = 292257
+
+    # This limit determines how many emails will be sent.
+    limit = 10
+
+    # Set the credentials.
     creds = set_creds()
+
+    # Instatiate LS remote control.
     ls = LimeSurvey(creds.url, creds.username)
+
+    # Start connection.
     ls.open(creds.password)
+
+    # Fetch participants with unsent invitations and uncompleted responses.
     res = ls.token.list_participants(
-        292257,
-        limit=10,
+        survey_id,
+        limit=limit,
         conditions={"sent": "N", "completed": "N"}
     )
+
+    # Grab the token ids only.
     token_ids = [part["tid"] for part in res]
+
+    # Send invitations with the token ids.
     # ls.token.invite_participants(618941, token_ids)
     print(res)
     print("correus enviats.")
+
+    # End connection to LS
     ls.close()
 
 
