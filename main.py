5ae348ea0787c5a26daff8a63eb2ed159a13c3cc
miquel <kamanyo@gmail.com>
2023-06-16 17:45:58 +0200

    Error handling for empty responses.

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 """Limesurvey invitation sender."""
 
 from limesurveyrc2api.limesurvey import LimeSurvey
+from limesurveyrc2api.exceptions import LimeSurveyError
 from credentials import set_creds
 
 
-def run():
+def send_invites():
     """Fetch a number of participants from a survey and sends them the invite."""
-
     # Configuration:
     # Survey to fetch from.
-    survey_id = 292257
+    survey_id = 618941
 
     # This limit determines how many emails will be sent.
     limit = 10
 
+    # Fetch participants with unsent invitations and uncompleted responses.
+    conditions = {"sent": "N", "completed": "N"}
+
+    try:
+        res = ls.token.list_participants(
+            survey_id,
+            limit=limit,
+            conditions=conditions
+        )
+
+        # Grab the token ids only.
+        token_ids = [part["tid"] for part in res]
+
+        # Send invitations with the token ids.
+        result = ls.token.invite_participants(survey_id, token_ids)
+        print(result)
+    except LimeSurveyError as e:
+        print(f"Error: {e}")
+        return
+
+
+if __name__ == "__main__":
     # Set the credentials.
     creds = set_creds()
 
@@ -22,25 +44,7 @@ def run():
 
     # Start connection.
     ls.open(creds.password)
+    send_invites()
 
-    # Fetch participants with unsent invitations and uncompleted responses.
-    res = ls.token.list_participants(
-        survey_id,
-        limit=limit,
-        conditions={"sent": "N", "completed": "N"}
-    )
-
-    # Grab the token ids only.
-    token_ids = [part["tid"] for part in res]
-
-    # Send invitations with the token ids.
-    # ls.token.invite_participants(618941, token_ids)
-    print(res)
-    print("correus enviats.")
-
-    # End connection to LS
+    # End connection to LS.
     ls.close()
-
-
-if __name__ == "__main__":
-    run()
