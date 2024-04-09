a9a20cbaca92b1229843694c33f229059f1d5f60
Miquel Caamaño Salom <kamanyo@gmail.com>
2024-04-09 15:16:02 +0200

    Added poetry as manager and installed Pyperclip.

---
+++ README.md
@@ -0,0 +1,2 @@
+# LS-Export-Files
+A fast way to download participant's documents from a survey.
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from limesurveyrc2api.limesurvey import LimeSurvey
-from collections import OrderedDict
-import os
-from dotenv import load_dotenv
 import base64
+import os
 import time as t
+from collections import OrderedDict
+
+import pyperclip  # type:ignore
+from dotenv import load_dotenv  # type:ignore
+from limesurveyrc2api.limesurvey import LimeSurvey  # type:ignore
 
 load_dotenv()
+
 
 def log_time(func):
     def wrapper():
@@ -14,7 +17,9 @@ def log_time(func):
         elapsed_time = t.time() - start_execution
         print(
             f"Function: {func.__name__}    "
-            f"Time: It took {elapsed_time} seconds.")
+            f"Time: It took {elapsed_time} seconds."
+        )
+
     return wrapper
 
 
@@ -33,46 +38,52 @@ def run():
     password = os.getenv("password")
     GENERAL_FORM_ID = 292257
 
-    # Open a session.
     api = LimeSurvey(url=url, username=username)
     api.open(password=password)
 
     # Participant token to search for.
-    token = input("Token de l'alumne: ")
+    token_list = list(pyperclip.paste())
+    print(token_list)
 
-    al_data = api.token.get_participant_properties(
-        GENERAL_FORM_ID, token_query_properties={"token": token})
+    for token in token_list:
+        al_data = api.token.get_participant_properties(
+            GENERAL_FORM_ID, token_query_properties={"token": token}
+        )
 
-    params = OrderedDict([
-        ("sessionkey", api.session_key),
-        ("surveyid", al_data["attribute_4"]),
-        ("token", token)
-    ])
+        params = OrderedDict(
+            [
+                ("sessionkey", api.session_key),
+                ("surveyid", al_data["attribute_4"]),
+                ("token", token),
+            ]
+        )
 
-    # Get a list of all the files related to that participant
-    cicle_response = api.query("get_uploaded_files", params)
-    cicle_files = ensure_dictionary(cicle_response)
+        # Get a list of all the files related to that participant
+        cicle_response = api.query("get_uploaded_files", params)
+        cicle_files = ensure_dictionary(cicle_response)
 
-    # TODO: Extract files form the general form.
-    params["surveyid"] = GENERAL_FORM_ID
-    general_response = api.query("get_uploaded_files", params)
-    general_files = ensure_dictionary(general_response)
+        # TODO: Extract files form the general form.
+        params["surveyid"] = GENERAL_FORM_ID
+        general_response = api.query("get_uploaded_files", params)
+        general_files = ensure_dictionary(general_response)
 
+        results = {**cicle_files, **general_files}
 
-    results = {**cicle_files, **general_files}
+        for [key, value] in results.items():
+            # Check if table exists
+            if key == "status":
+                continue
 
-    for [key, value] in results.items():
-        # Check if table exists
-        if key == "status":
-            continue
-
-        # Decode file
-        with open(f"fitxers exportats/{al_data['lastname']}, {al_data['firstname']} - {value['meta']['question']['title']}.pdf", "wb") as f:
-            f.write(base64.b64decode(value["content"]))
+            # Decode file
+            with open(
+                f"fitxers exportats/{al_data['lastname']}, {al_data['firstname']} - {value['meta']['question']['title']}.pdf",
+                "wb",
+            ) as f:
+                f.write(base64.b64decode(value["content"]))
 
     # Close the session.
     api.close()
 
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     run()
